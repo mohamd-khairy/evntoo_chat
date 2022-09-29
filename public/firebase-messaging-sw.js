ed5321e1b2e4d448deee59dba108bfa1b1ee66c3
Samar Samir Ragab <95422450+SamarSamirRagab@users.noreply.github.com>
2022-09-29 10:44:09 +0200

    fix

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -13,18 +13,18 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-const messaging = firebase.messaging();
+// const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    // Customize notification here
-    const { title, body } = payload.notification;
+// messaging.onBackgroundMessage((payload) => {
+//     console.log('[firebase-messaging-sw.js] Received background message ', payload);
+//     // Customize notification here
+//     const { title, body } = payload.notification;
 
-    const notificationTitle = 'Background Message Title';
-    const notificationOptions = {
-        body
-    };
+//     const notificationTitle = 'Background Message Title';
+//     const notificationOptions = {
+//         body
+//     };
 
-    self.registration.showNotification(notificationTitle,
-        notificationOptions);
-});
+//     self.registration.showNotification(notificationTitle,
+//         notificationOptions);
+// });
